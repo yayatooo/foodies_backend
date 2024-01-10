9c19c6e98e1b38ef40d275da1e770bc1660e07fe
Rahmat Hidayat <yayatbx@gmail.com>
2024-01-10 21:26:46 +0800

    fix router cart

--- app/cart/router/cartItemRouter.js
+++ app/cart/router/cartItemRouter.js
@@ -1,9 +1,25 @@
 import express from "express";
-import { updateCart, getItemCart } from "../controller/controllerCart.js";
-import { policeCheck } from "../../middlewares/index.js";
+import {
+  updateCart,
+  getItemCart,
+  addCart,
+  increase,
+  decrease,
+  increaseByQty,
+  clearUserCart,
+  getItemCartById,
+  removeUserCart,
+} from "../controller/controllerCart.js";
 const cartItemRouter = express.Router();
 
-cartItemRouter.put("/carts", policeCheck("update", "Cart"), updateCart);
+cartItemRouter.put("/carts", updateCart);
 cartItemRouter.get("/carts", getItemCart);
+cartItemRouter.post("/carts", addCart);
+cartItemRouter.patch("/carts/:userId/:id/decrease", decrease);
+cartItemRouter.patch("/carts/:userId/:id/increase", increase);
+cartItemRouter.post("/carts/:userId/:id/increase/:qty", increaseByQty);
+cartItemRouter.delete("/carts/:userId/:id/", removeUserCart);
+cartItemRouter.delete("/carts/:userId", clearUserCart);
+cartItemRouter.get("/carts/:userId", getItemCartById);
 
 export default cartItemRouter;
